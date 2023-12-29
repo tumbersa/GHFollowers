e5abbb486d452f34a6825ac6294f4639589541a8
tumbersa <gleb.kapustin.95@gmail.com>
2023-12-30 01:23:27 +0300

    Initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Глеб Капустин on 30.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
