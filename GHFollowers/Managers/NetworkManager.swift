72c19d9142bbbb827c74539765c55dea5b0e2329
tumbersa <gleb.kapustin.95@gmail.com>
2023-12-31 16:08:49 +0300

    Added download & cache avatar images

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Глеб Капустин on 30.12.2023.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared   = NetworkManager()
-    let baseUrl         = "https://api.github.com/users/"
+    private let baseUrl         = "https://api.github.com/users/"
+    let cache           = NSCache<NSString, UIImage>()
     
     private init() {}
     
